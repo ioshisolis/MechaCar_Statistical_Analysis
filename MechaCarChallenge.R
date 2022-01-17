b3f7ead666889239dc1ea3f129fbf2f5ef4d82af
Jorge Esteban Solis Franco <hola@soyjorgesolis.com>
2022-01-17 00:06:59 -0700

    deliverable1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,18 @@
+
+# Use the library() function to load the dplyr package.
+
+library(dplyr)
+
+
+# Import and read in the MechaCar_mpg.csv file as a dataframe.
+
+MechaCar_mpg_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
+head(MechaCar_mpg_df)
+
+# Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), and add the dataframe you created in Step 4 as the data parameter.
+# To create a linear regression model, our R statement would be as follows:
+  
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg_df) #create linear model
+
+
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg_df)) #generate summary statistics
